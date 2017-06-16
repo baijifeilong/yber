0ababf4ae9d7c843a65e396318f52eb7510cd186
SongJianWei <baijifeilong@gmail.com>
2017-06-16 18:22:50 +0800

    First commit

---
+++ src/main/java/xyz/ohmycs/bjwiber/Foo.java
@@ -0,0 +1,10 @@
+package xyz.ohmycs.bjwiber;
+
+/**
+ * Created by BaiJiFeiLong@gmail.com on 2017/6/16.
+ */
+public class Foo {
+    public static void greet() {
+        System.out.println("I am foo");
+    }
+}
